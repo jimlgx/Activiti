e183068738b02ee62b68a58a744997682d94ce90
junming.wang <junming.wang@qunar.com>
2015-05-30 19:59:39 +0800

    change mysql db

---
+++ modules/activiti-webapp-explorer2/src/main/java/org/activiti/explorer/filter/ExplorerExtFilter.java
@@ -0,0 +1,64 @@
+/*
+ * @project activiti-root
+ * @package org.activiti.explorer.filter
+ * @file ExplorerExtFilter.java
+ * @version  1.0
+ * @author  junming.wang
+ * @time 2015-05-30
+ */
+package org.activiti.explorer.filter;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * <code>ExplorerExtFilter</code>
+ *
+ * @author junming.wang
+ * @version 1.0
+ * @since 1.0 2015-05-30
+ */
+public class ExplorerExtFilter extends ExplorerFilter {
+    private List<String> ignoreList = new ArrayList<String>();
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        ignoreList.add("/druid");
+        ignoreList.add("/H2Console");
+
+    }
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        String path = req.getRequestURI().substring(req.getContextPath().length());
+        int indexSlash = path.indexOf("/", 1);
+        String firstPart = null;
+        if (indexSlash > 0) {
+            firstPart = path.substring(0, indexSlash);
+        } else {
+            firstPart = path;
+        }
+
+
+        if (isMatch(firstPart)) {
+            chain.doFilter(request, response); // Goes to default servlet.
+        } else {
+            super.doFilter(request,response,chain);
+        }
+    }
+
+    private boolean isMatch(String firstPart) {
+
+        return ignoreList.contains(firstPart);
+    }
+
+
+}
