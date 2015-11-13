4796b1a324df9de502381df5a5de9d72d72ddd24
junming.wang <junming.wang@qunar.com>
2015-11-13 18:55:07 +0800

    fix explorerExtFilter

--- modules/activiti-webapp-explorer2/src/main/java/org/activiti/explorer/filter/ExplorerExtFilter.java
+++ modules/activiti-webapp-explorer2/src/main/java/org/activiti/explorer/filter/ExplorerExtFilter.java
@@ -35,6 +35,7 @@ public class ExplorerExtFilter extends ExplorerFilter {
     private static final Logger LOG = LoggerFactory
             .getLogger(ExplorerExtFilter.class);
     public static final String EXPLORER2 = "/explorer2";
+    public static final String EXPLORER2_ = "/explorer2/";
 
     private List<String> ignoreExtList = new ArrayList<String>();
 
@@ -75,17 +76,21 @@ public class ExplorerExtFilter extends ExplorerFilter {
 //        return ignoreExtList.contains(firstPart);
 //    }
     public void superDoFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
         HttpServletRequest req = (HttpServletRequest) request;
         HttpServletResponse resp = (HttpServletResponse) response;
         String path = req.getRequestURI().substring(req.getContextPath().length());
         String firstPart = StringUtils.substringBefore(path,"/");
         if (ignoreExtList.contains(firstPart)) {
             chain.doFilter(request, response); // Goes to default servlet.
-        } else if(!EXPLORER2.equals(path)&&StringUtils.startsWith(path, EXPLORER2)) {
+        } else if(!EXPLORER2.equals(path)&&!EXPLORER2_.equals(path) && StringUtils.startsWith(path, EXPLORER2)) {
             String path2 = StringUtils.removeStart(path, EXPLORER2);
             String path３ = StringUtils.substringBefore(path2,"/");
-            ignoreExtList.contains(path３);
-            request.getRequestDispatcher(path３).forward(request, response);
+            if(ignoreExtList.contains(path３)){
+                chain.doFilter(request, response); // Goes to default servlet.
+            }else{
+                request.getRequestDispatcher(path３).forward(request, response);
+            }
         } else {
             request.getRequestDispatcher("/ui" + path).forward(request, response);
         }
